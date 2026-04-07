158a36d5921aa4ceebbfc4fba78717f9c88a644e
Mr-subrat-01 <skumarsahoo470@gmail.com>
2026-04-07 14:55:56 +0530

    initial commit

---
+++ BasicCoding.cs
@@ -0,0 +1,107 @@
+namespace BasicCodingForDSA{
+    public class BasicCoding {
+// 1. Take a number and print whether it’s positive, negative, or zero.
+        public void CheckNumber() {
+            Console.WriteLine("Enter an number :");
+            int n = int.Parse(Console.ReadLine());
+            if(n==0){
+                Console.WriteLine("Zero");
+            }else if(n<0){
+                Console.WriteLine("Negative");
+            }else{
+                Console.WriteLine("Positive");
+            }
+        }
+
+// 2. Check if a number is even or odd.
+        public void CheckEvenOdd(){
+            Console.WriteLine("Enter an Number :");
+            int n = int.Parse(Console.ReadLine());
+            if(n%2==0){
+                Console.WriteLine("Even Number");
+            }else{
+                Console.WriteLine("Odd Number");
+            }
+        }
+
+// 3. Check if a number is divisible by 5.
+        public void CheckDivisibleBy5(){
+            Console.WriteLine("Enter an Number :");
+            int n = int.Parse(Console.ReadLine());
+            if(n%5==0){
+                Console.WriteLine("Yuup!! The Number: "+n+" is divisble by 5");
+            }else{
+                Console.WriteLine("Noop!! The number: "+n+" is not divisble by 5");
+            }
+        }
+
+// 4. Check if a number is divisible by both 3 and 5.
+        public void CheckDivisibleByBoth5and3(){
+            Console.WriteLine("Enter an number");
+            int n = int.Parse(Console.ReadLine());
+            if(n%3 ==0 && n%5==0){
+                Console.WriteLine("Yuup!! its divisible by both 3 and 5");
+            }else{
+                Console.WriteLine("Noop!! its not divisble by both 3 and 5");
+            }
+        }
+
+// 5. Check if a given year is a leap year.
+        public void CheckIsLeapYear(){
+            Console.WriteLine("Enter an number:");
+            int n = int.Parse(Console.ReadLine());
+            // 1. Divisible by 400 or divisible by 4 but not by 100 
+            if((n%400==0) || (n%4 ==0 && n%100 !=0)){
+                Console.WriteLine("Its Leap Year!!");
+            }else{
+                Console.WriteLine("No!, Its not a Leap Year!!");
+            }
+        }
+
+// 6. Take two numbers and print the larger one.
+    public void CheckLargeIn2Number(){
+        Console.WriteLine("Enter first number :");
+        int n1=int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Second number: ");
+        int n2=int.Parse(Console.ReadLine());
+        if(n1>n2){
+            Console.WriteLine("Largest Number is : "+n1);
+        }else{
+            Console.WriteLine("Largest Number is : "+n2);
+        }
+    }
+
+// 7. Take three numbers and print the largest.
+    public void CheckLargeIn3Number(){
+        Console.WriteLine("Enter 3 number comma separted: ");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(",",StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries);
+        if(numbers.Length==3){
+            int n1 = int.Parse(numbers[0]);
+            int n2 = int.Parse(numbers[1]);
+            int n3 = int.Parse(numbers[2]);
+            if(n1>n2 && n1 > n3){
+                Console.WriteLine("Largest Number is : "+n1);
+            }else if(n2>n3 && n2 > n1){
+                Console.WriteLine("Largest Number is :"+n2);
+            }else{
+                Console.WriteLine("Largest Number is :"+n3);
+            }
+            // another built-in approch using Math.Max() function its take two value to check large 
+            Console.WriteLine("Largest Number is : "+Math.Max(n1,Math.Max(n2,n3)));
+        }else{
+            return;
+        }
+    }
+
+// 8. Take a temperature value and print “Cold”, “Warm”, or “Hot” using range conditions.
+    public void CheckWeather(){
+        Console.WriteLine("Enter temperature in format like (e.g. 30F or 20C)");
+        string input = Con
+    }
+// 9. Take a character and check if it’s a vowel or consonant.
+// 10. Take a character and check whether it’s uppercase, lowercase, a digit, or a special
+// character.
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using  BasicCodingForDSA;
+public class Program{
+    public static void Main(string[] args){
+            var bs = new BasicCoding();
+        while(true){
+            try {
+                Console.WriteLine("=============================");
+                Console.WriteLine("Choose your choice: ");
+                Console.WriteLine("1. Check Number: ");
+                Console.WriteLine("2. Check EvenOdd: ");
+                Console.WriteLine("3. Check Divisible By 5: ");
+                Console.WriteLine("4. Check Divisible By both 3 and 5 : ");
+                Console.WriteLine("5. Check Leap Year : ");
+                Console.WriteLine("6. Check Large In 2 Number: ");
+                Console.WriteLine("7. Check Large In 3 Number: ");
+                Console.WriteLine("0. Exit: ");
+                Console.WriteLine("=============================");
+                int c = int.Parse(Console.ReadLine());
+                switch(c) {
+                    case 1:
+                        bs.CheckNumber();
+                    break;
+                    case 2:
+                        bs.CheckEvenOdd();
+                    break;
+                    case 3:
+                        bs.CheckDivisibleBy5();
+                    break;
+                    case 4:
+                        bs.CheckDivisibleByBoth5and3();
+                    break;
+                    case 5:
+                        bs.CheckIsLeapYear();
+                    break;
+                    case 6:
+                        bs.CheckLargeIn2Number();
+                    break;
+                    case 7:
+                        bs.CheckLargeIn3Number();
+                    break;
+                    case 0:
+                        Console.WriteLine("Thank You!!");
+                    return;
+                    default:
+                        Console.WriteLine("Invalid Choice!");
+                    break;
+                }
+            }
+            catch(Exception ex){
+                Console.WriteLine("Error : "+ ex.Message+" Try Again!! ");
+            }
+        }
+    }
+}
